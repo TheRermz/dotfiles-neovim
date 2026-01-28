4aeb03bc8c7d362a2749d3df771ac3dedd9935f7
Murilo <mufischer@hotmail.com>
2026-01-27 22:12:21 -0300

    transmite a opacidade do terminal pro neovim

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,16 +1,16 @@
-return{
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        integrations = {
-          rainbow_delimiters = true
-        }
-      })
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+				integrations = {
+					rainbow_delimiters = true,
+				},
+			})
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
-
